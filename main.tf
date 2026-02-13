d7d89e12aece3338eaf1ef3ddb5e46808af3168f
Chay1711 <chayajayanu@gmail.com>
2026-02-13 19:51:21 +0530

    added filessss

---
+++ main.tf
@@ -0,0 +1,9 @@
+provider "aws" {
+  region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "Example33" {
+  ami = "ami-0c1fe732b5494dc14"  # Specify an appropriate AMI ID
+    instance_type = "t2.micro"
+    subnet_id = "subnet-01ed0bf481ab409a2"#ami
+}

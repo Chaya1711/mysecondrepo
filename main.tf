2ad78a541a2852e58bedb65378e812974d376b86
Chay1711 <chayajayanu@gmail.com>
2026-02-17 01:36:29 +0530

    added changes to

--- main.tf
+++ main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 resource "aws_instance" "Example33" {
   ami = "ami-0c1fe732b5494dc14"  # Specify an appropriate AMI ID
     instance_type = "t2.micro"
-    subnet_id = "subnet-01ed0bf481ab409a2"#subnet
+    subnet_id = "subnet-01ed0bf481ab409a2"#subnet id
 }

40b77f00a633a80dc996b72a448071b1285b5a4a
Chay1711 <chayajayanu@gmail.com>
2026-02-17 01:55:14 +0530

    added changes to .yml

--- main.tf
+++ main.tf
@@ -4,6 +4,6 @@ provider "aws" {
 
 resource "aws_instance" "Example33" {
   ami = "ami-0c1fe732b5494dc14"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro" #instance types
+    instance_type = "t3.micro" #instance types
     subnet_id = "subnet-01ed0bf481ab409a2"#subnet id 
 }
